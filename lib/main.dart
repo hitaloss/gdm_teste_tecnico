df5557ad80a52e86669fcf8ce64318f006c7cda2
hitalo <hitalo.2014.hs@gmail.com>
2025-02-11 22:22:59 -0300

    feat: applied providers required settings along with loading indicator for http requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gdm_teste_tecnico/screens/motels.dart';
 
-void main() {
+import 'package:gdm_teste_tecnico/screens/motels.dart';
+import 'package:gdm_teste_tecnico/providers/motels_data_provider.dart';
+
+void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final motels = ref.watch(motelsProvider);
+
+    Widget content = const Scaffold(
+        body: Center(
+            child: CircularProgressIndicator(
+      color: Color.fromARGB(255, 242, 8, 45),
+    )));
+
+    if (motels != null) {
+      content = const MotelsScreen();
+    }
+
     return MaterialApp(
-        title: 'Guia de Motéis go',
-        theme: ThemeData(
-          useMaterial3: true,
-          splashFactory: NoSplash.splashFactory,
-        ),
-        home: const MotelsScreen());
+      title: 'Guia de Motéis go',
+      theme: ThemeData(
+        useMaterial3: true,
+        splashFactory: NoSplash.splashFactory,
+      ),
+      home: content,
+    );
   }
 }
